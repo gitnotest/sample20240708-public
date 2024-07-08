df147ed23f04fcf7dace02521b6d5eb53355d715
Hara, Rumi <rumi.hara@hpe.com>
2024-07-08 17:03:38 +0900

    20240708b

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -16,7 +16,7 @@ public class HelloworldApplication {
 	class HelloworldController {
 		@GetMapping("/")
 		String hello() {
-		return "Hello " + name + " 20240708a!";
+		return "Hello " + name + " 20240708b!";
 		}
 	}
 	public static void main(String[] args) {
